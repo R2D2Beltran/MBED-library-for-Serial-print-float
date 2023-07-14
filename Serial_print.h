d695d66d5fa8ca57b7428e159b131c152a982e1a
R2D2Beltran <arturo_2486_beltran@hotmail.com>
2023-07-14 19:28:23 +0000

    inclusion de funciones printf y printd

---
+++ Serial_print.h
@@ -0,0 +1,2 @@
+#define baud_rate1 115200
+#define baud_rate2 9600
--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include "mbed.h"
-#include "envio_de_flotante.cpp"
+#include "Serial_print.cpp"
+#include "Serial_print.h"
+#include <cstdint>
+
 // main() runs in its own thread in the OS
 int main()  
 {   
-    pc.set_baud(115200);
+    //Ambas velocidades de transmision deben ser las mismas
+    Serialf serialf=Serialf(baud_rate1); //establecer baud_rate creacion de objeto serialf para envio de numeros flotantes
+    Seriald seriald=Seriald(baud_rate1); //establecer baud_rate creacion de objeto seraild para envio de enteros
     while (true) {
-        float flotan=86.912;
-        int enter=86;
-        Serialf arur(flotan); //envio de numero flotante
-        Seriald artur(enter); //envio de numero entero
         
-        /*for (float i = 0.0; i < 360.0; i += 1){
-                float senoo=0.5 * (sinf(i * 3.14159265 / 180.0))*1000000;
-                 Seriald asi(senoo);
-        }*/
+        float flotan=8100.912;
+        int i=56;
+        serialf.printf(-164.092); //imprime flotante con tres decimales y hace salto de linea
+        serialf.printf(flotan); 
+        seriald.printd(-i);       //imprime entero y hace salto de linea
+        seriald.printd(-89.092);    //trunca imprime y hace salto de linea
+        
+        /*
+        for (float i = 0.0; i < 360.0; i += 1){
+                int senoo=0.5 * (sinf(i * 3.14159265 / 180.0))*999999999;//1000000000  999999999
+                 seriald.printd(senoo);
+        }
+        */
+        //ThisThread::sleep_for(500ms);
     }
 }
 
