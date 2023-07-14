d5ae85ff7377154e15c459c1c6ac4766d3693efd
R2D2Beltran <arturo_2486_beltran@hotmail.com>
2023-07-14 16:43:45 +0000

    Impresion de flotante y enteros

--- envio_de_flotante.cpp
+++ envio_de_flotante.cpp
@@ -3,7 +3,7 @@
 static BufferedSerial pc(USBTX,USBRX);
 
 class Serial{
-    private:
+    protected:
         float flotante=0;
         int parte_Entera=0;
         int parte_Decimal=0;
@@ -19,23 +19,27 @@ class Serial{
     public:
         //constructor que recibe el numero flotante y cantidad de caracteres maxima del flotante
         Serial(float f) : flotante (f){
-        float_to_int();
-        count_digitos();
-        send_to_portSerial(); 
+        
         }
     
-    private:
-        void float_to_int(){
-            aux_flotante=abs(flotante);
-            parte_Entera=int(aux_flotante);
-            if(flotante>=0){
-            parte_Decimal=int((flotante-parte_Entera)*1000);
-            }else{
-            parte_Decimal=int(abs((flotante+parte_Entera)*1000));    
-            }
+    protected:
+
+        virtual void float_to_int()=0;
+        virtual void count_digitos()=0;
+        virtual void send_to_portSerial()=0;
+
+};
+
+class Serialf : public Serial{
+    public: 
+        Serialf (float f) : Serial(f){
+            float_to_int();
+            count_digitos();
+            send_to_portSerial(); 
         }
 
-        void count_digitos(){
+    protected:
+    void count_digitos() override{
 
             aux_entero=parte_Entera;
             aux_decimal=parte_Decimal;
@@ -59,37 +63,86 @@ class Serial{
             }
             cantidad_de_caracteres=count_int+count_dec+4;
 
-        }
+    }   
 
-        void send_to_portSerial(){
-            if(flotante>=0){    
-                if (parte_Decimal>=100){
-                    char mensaje[(cantidad_de_caracteres)];
-                    sprintf(mensaje,"%d.%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }else if(parte_Decimal<100 && parte_Decimal>=10){
-                    char mensaje[(cantidad_de_caracteres+1)];
-                    sprintf(mensaje,"%d.0%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }else{
-                    char mensaje[(cantidad_de_caracteres+2)];
-                    sprintf(mensaje,"%d.00%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }
+    void float_to_int()override{
+    
+        aux_flotante=abs(flotante);
+        parte_Entera=int(aux_flotante);
+        if(flotante>=0){
+        parte_Decimal=int((flotante-parte_Entera)*1000);
+        }else{
+        parte_Decimal=int(abs((flotante+parte_Entera)*1000));    
+        }
+    } 
+
+    void send_to_portSerial()override{
+        if(flotante>=0){    
+            if (parte_Decimal>=100){
+                char mensaje[(cantidad_de_caracteres)];
+                sprintf(mensaje,"%d.%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
+            }else if(parte_Decimal<100 && parte_Decimal>=10){
+                char mensaje[(cantidad_de_caracteres+1)];
+                sprintf(mensaje,"%d.0%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
             }else{
-                if (parte_Decimal>=100){
-                    char mensaje[(cantidad_de_caracteres+1)];
-                    sprintf(mensaje,"-%d.%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }else if(parte_Decimal<100 && parte_Decimal>=10){
-                    char mensaje[(cantidad_de_caracteres+2)];
-                    sprintf(mensaje,"-%d.0%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }else{
-                    char mensaje[(cantidad_de_caracteres+3)];
-                    sprintf(mensaje,"-%d.00%d\n\r", parte_Entera,parte_Decimal);
-                    pc.write(mensaje,sizeof(mensaje));
-                }
+                char mensaje[(cantidad_de_caracteres+2)];
+                sprintf(mensaje,"%d.00%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
+            }
+        }else{
+            if (parte_Decimal>=100){
+                char mensaje[(cantidad_de_caracteres+1)];
+                sprintf(mensaje,"-%d.%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
+            }else if(parte_Decimal<100 && parte_Decimal>=10){
+                char mensaje[(cantidad_de_caracteres+2)];
+                sprintf(mensaje,"-%d.0%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
+            }else{
+                char mensaje[(cantidad_de_caracteres+3)];
+                sprintf(mensaje,"-%d.00%d\n\r", parte_Entera,parte_Decimal);
+                pc.write(mensaje,sizeof(mensaje));
+            }
+        }        
+    }
+        
+};
+
+class Seriald : public Serial{
+    public:
+        Seriald (int d) : Serial (d){
+            float_to_int();
+            count_digitos();
+            send_to_portSerial();
+        }
+    protected:
+    void float_to_int()override{
+        parte_Entera=int(flotante);
+        aux_entero=int(abs(flotante));
+    }
+    void count_digitos()override{
+        
+        if (aux_entero == 0) {
+                count_int = 1;
+        } else {
+            while (aux_entero > 0) {
+                aux_entero /= 10;
+                count_int++;
             }
         }
+        cantidad_de_caracteres=count_int+2;
+    }
+    void send_to_portSerial()override{
+        if(flotante>=0){
+            char mensaje[cantidad_de_caracteres];
+            sprintf(mensaje,"%d\n\r",parte_Entera);
+            pc.write(mensaje,sizeof(mensaje));
+        }else{
+            char mensaje[cantidad_de_caracteres+1];
+            sprintf(mensaje,"%d\n\r",parte_Entera);
+            pc.write(mensaje,sizeof(mensaje));
+        }
+    }
 };
--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 // main() runs in its own thread in the OS
 int main()  
 {   
-    
+    pc.set_baud(115200);
     while (true) {
-        float float_to_send=76.912;
-        Serial (float_to_send*1);
-        /*
-        for (float i = 0.0; i < 360.0; i += 1){
-                 Serial((0.5 * (sinf(i * 3.14159265 / 180.0) + 1))*1000);
-        }
-        */
+        float flotan=86.912;
+        int enter=86;
+        Serialf arur(flotan); //envio de numero flotante
+        Seriald artur(enter); //envio de numero entero
         
+        /*for (float i = 0.0; i < 360.0; i += 1){
+                float senoo=0.5 * (sinf(i * 3.14159265 / 180.0))*1000000;
+                 Seriald asi(senoo);
+        }*/
     }
 }
 
